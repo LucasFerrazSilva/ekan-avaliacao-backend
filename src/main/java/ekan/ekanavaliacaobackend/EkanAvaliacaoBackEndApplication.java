1cd04e9f31e6ad900ac66548368347af3d515037
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2023-06-16 10:00:43 -0300

    First commit

---
+++ src/main/java/ekan/ekanavaliacaobackend/EkanAvaliacaoBackEndApplication.java
@@ -0,0 +1,13 @@
+package ekan.ekanavaliacaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkanAvaliacaoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkanAvaliacaoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ekan/ekanavaliacaobackend/EkanAvaliacaoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ekan.ekanavaliacaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkanAvaliacaoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
